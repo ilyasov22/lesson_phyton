c82c31be09f6cac4b61fa9626f8efc5d7741d782
Ilyasov.n <ilyasov.nu@yandex.ru>
2024-04-12 20:38:20 +0300

    dmopc16c1p0, C.C. and Cheese-kun

--- chapter_2/C.C._and_Cheese-kun_dmopc16c1p0.py
+++ chapter_2/C.C._and_Cheese-kun_dmopc16c1p0.py
@@ -1,16 +1,28 @@
 # dmopc16c1p0, C.C. and Cheese-kun
 
-# C.C. will be absolutely satisfied if the pizza she gets has a width of
-#  units and an extra-cheesiness of at least
-# C.C. will be fairly satisfied if the pizza she gets has a width of
-#  unit and an extra-cheesiness of at most
+# C.C. will be absolutely satisfied if the pizza she gets has a width of 3
+#  units and an extra-cheesiness of at least 95%
+# C.C. will be fairly satisfied if the pizza she gets has a width of 1
+#  unit and an extra-cheesiness of at most 50 %
 # C.C. will be very satisfied with any other pizza she receives.
 
 # W : (1 <= W <= 3)
 # C : (0 <= C <= 100)
 # M : C.C. is M satisfied with her pizza.
 
-width = int(input())
-cheese = int(input())
-Make
+# w = 3 c >= 95 absolutely
+# w = 1 c <= 50 fairly
+# (w != 3 and (not c > 95) or (w != 1 and (not c <= 50))   very
+
+w = int(input())
+c = int(input())
+
+if w == 3 and c >= 95:
+   M = 'absolutely'
+elif w == 1 and c <= 50:
+   M = 'fairly'
+else:
+   M = 'very'
+
+print('C.C. is ' + str(M) + ' satisfied with her pizza.')
 
