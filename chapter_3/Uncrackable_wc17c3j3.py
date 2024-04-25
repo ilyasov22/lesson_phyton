369fd569e259698cd7421647a125b208125fbc2c
ilyasov.nu <ilyasov.nu@gmail.com>
2024-04-26 02:15:10 +0300

    wc17c3j3, Uncrackable

--- chapter_3/Uncrackable_wc17c3j3.py
+++ chapter_3/Uncrackable_wc17c3j3.py
@@ -1,12 +1,27 @@
 # wc17c3j3, Uncrackable
 
-# пароль должен быть от 8 до 12 символов
+# пароль должен быть между 8 и 12 символов
 # не менее 3х нижний регистр
 # не менее 2х верхний регистр
 # не менее 1 цыфры
 # вывод : Valid или Invalid
 
-pass = input()
+passw = input()
 
-for login in pass:
-    if
+lower = 0
+upper = 0
+digit = 0
+
+if 8 <= len(passw) <= 12:
+    for login in passw:
+        if login.islower():
+            lower = lower + 1
+        elif login.isupper():
+            upper = upper + 1
+        elif login.isdigit():
+            digit = digit + 1
+
+if upper >= 2 and lower >= 3 and digit >= 1:
+   print("Valid")
+else:
+   print("Invalid")
