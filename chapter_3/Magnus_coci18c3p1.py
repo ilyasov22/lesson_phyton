f66e0f2fdef98819e0ff20f56fa9d1d5d77a2f69
Ilyasov.n <ilyasov.nu@yandex.ru>
2024-04-26 21:00:38 +0300

    coci18c3p1, Magnus

--- chapter_3/Magnus_coci18c3p1.py
+++ chapter_3/Magnus_coci18c3p1.py
@@ -1,2 +1,11 @@
 # coci18c3p1, Magnus
 
+title = 'The Escape'
+total = 0
+for char1 in title:
+    print(char1)
+    for char2 in title:
+        print(char2)
+        total = total + 1
+print(total)
+
