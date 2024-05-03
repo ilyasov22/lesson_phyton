0d7ceea5ded61f6e4175c6e00b1e5c298efde1be
Ilyasov.n <ilyasov.nu@yandex.ru>
2024-05-03 21:37:26 +0300

    coci18c3p1, Magnus

--- chapter_3/Magnus_coci18c3p1.py
+++ chapter_3/Magnus_coci18c3p1.py
@@ -2,11 +2,22 @@
 # в строке надо найти блок со словом HONI и вывести сколько раз это блок попадается в строке
 
 world = input()
-block = 0
-#'H' + 'O' + 'N' + 'I'
 
-for b in world:
-    if b == 'H' and b == 'O' and b == 'N' and b == 'I'
-       block = block + 1
-       total = len(b)
+block = 0
+total = 0
+
+for honi in world:
+    if block == 0:
+        match = 'H'
+    elif block == 1:
+        match = 'O'
+    elif block == 2:
+        match = 'N'
+    else:
+        match = 'I'
+    if match == honi:
+        block = block + 1
+        if block == 4:
+            block = 0
+            total = total + 1
 print(total)
